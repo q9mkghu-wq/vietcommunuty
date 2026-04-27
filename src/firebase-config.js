93daada0610d92171ff9ed553c7a587a9e55072e
q9mkghu-wq <q9mkghu@gmail.com>
2026-04-27 12:24:52 +0900

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
- apiKey: "AIzaSyDWG4CwIzQe0LheL2pMXE_XINo4pw0s4oQ",
-  authDomain: "vietcommunity.firebaseapp.com",
-  projectId: "vietcommunity",
-  storageBucket: "vietcommunity.firebasestorage.app",
-  messagingSenderId: "751701113900",
-  appId: "1:751701113900:web:bfb100999e27f84d67e589"
+  apiKey: "AIzaSyCmPJyCnU60NQSX22icwxmyMabrI6y6FW8",
+  authDomain: "vietcommunity-199d6.firebaseapp.com",
+  projectId: "vietcommunity-199d6",
+  storageBucket: "vietcommunity-199d6.firebasestorage.app",
+  messagingSenderId: "31789592036",
+  appId: "1:31789592036:web:e88a9e8b26188bd14f6bf1"
 };
 
 const app = initializeApp(firebaseConfig);
